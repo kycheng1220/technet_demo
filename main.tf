4c5b5b7ce9d360b9c795eb1bb808d0d88304d031
Ken Cheng <34132064+kycheng1220@users.noreply.github.com>
2022-07-06 21:43:22 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,69 @@
+# ---------------------------------------------------------------------------------------------------------
+# This demo template launches an AWS EC2 instance with a websevber serving a simple index.html page showing "Hello Technet"
+#
+# To access the "Hello Technet" page:
+#    type "http://<EC2 instance's public ip address>:8080" in your browser address bar
+#    or type "curl http://<EC2 instance's public ip address>:8080/" in any CLI
+# ---------------------------------------------------------------------------------------------------------
+
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "< 4.0"
+    }
+  }
+}
+
+# ------------------------------------------------------------------------------
+# Select the aws region where the instance will be launched
+# ------------------------------------------------------------------------------
+provider "aws" {
+  region = "us-west-2"
+}
+
+# -------------------------------------------------------------------------------
+# Deploy a EC2 instance
+# -------------------------------------------------------------------------------
+
+resource "aws_instance" "technet-demo" {
+  # Ubuntu Server 18.04 LTS (HVM), SSD Volume Type in us-west-2
+  ami                    = "ami-0b152cfd354c4c7a4"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello Technet" > index.html
+                nohup busybox httpd -f -p "8080" &
+                EOF
+
+  tags = {
+    Name = "technet-demo"
+  }
+}
+
+# -------------------------------------------------------------------------------
+# Create the secruity group for the EC2 instance
+# -------------------------------------------------------------------------------
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound HTTP from anywhere
+  egress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
